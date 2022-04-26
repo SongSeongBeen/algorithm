8823449f6c2d053a212b875b779aeff1db5250ce
송성빈 <aktm1004@naver.com>
2022-04-26 21:56:46 +0900

    03

---
+++ src/com/javaex/algorithm/Solution03.java
@@ -0,0 +1,30 @@
+package com.javaex.algorithm;
+
+import java.util.stream.IntStream;
+
+public class Solution03 {
+		public int solution(int[] budgets, int M){
+			
+			int min = 0;
+			int max = IntStream.of(budgets).max().orElse(0);
+			int answer = 0;
+			
+			while(min <= max) {
+				
+				final int mid = (min +max) /2;
+				int sum = IntStream.of(budgets)
+					.map(b -> Math.min(b,mid))
+					.sum();
+					
+				if(sum <= M){
+					min =mid +1;
+					answer = mid;
+				} else {
+					max = mid -1;
+				}
+			}
+			
+			return answer;
+		}
+
+}
