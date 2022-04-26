8a456962629004f20355193b26e93bef12bd1b8c
송성빈 <aktm1004@naver.com>
2022-04-26 22:16:54 +0900

    01

--- src/com/javaex/algorithm/Solution01.java
+++ src/com/javaex/algorithm/Solution01.java
@@ -1,26 +1,28 @@
 package com.javaex.algorithm;
 
 public class Solution01 {
-	public int solution(int n, int[] stations, int w){
-		
-	    int answer = 0;
-	    int index = 0;
-	    int dong = 1;
-	
-	    while(dong <= n){
-	        if(index < stations.length && dong >= stations[index] - w ){
-	            dong = stations[index] + w + 1;
-	            index ++;
-	        }
-	        else {
-	            answer += 1;
-	            dong += (w*2) +1;
-	
-	        }
-	    }
-	    System.out.println(answer);
-	    return answer;
-	   
-	}
-	
+        public int solution(int n, int[] stations, int w) {
+
+            int answer = 0;
+            int prevRight = 0;
+            for (int i = 0; i < stations.length; i++) {
+                int curLeft = stations[i] - w;
+                int curRight = stations[i] + w;
+                int dist = curLeft - prevRight -1;
+                if(dist > 0){
+                    //기지국 사이에 빈 공간이 있는 경우
+                    answer += Math.ceil((double)dist / (2*w+1));
+                }
+                prevRight = curRight;
+            }
+
+            int finalDist = n - prevRight;
+            if(finalDist > 0){
+                //기지국 사이에 빈 공간이 있는 경우
+                answer += Math.ceil((double)finalDist / (2*w+1));
+            }
+
+            return answer;
+        }
+
 }
