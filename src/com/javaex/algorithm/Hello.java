ff4f4e9138b64e7b91ad88ceba3b3582d36d51ab
송성빈 <aktm1004@naver.com>
2022-05-05 23:01:14 +0900

    algorithm

---
+++ src/com/javaex/algorithm/Hello.java
@@ -0,0 +1,14 @@
+package com.javaex.algorithm;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("\\    /\\");
+		sb.append("  )  ( ')");
+		sb.append(" (  /  )");
+		sb.append(" \\(__)|");
+	}
+	
+}
